6d4fa01e70cf5126d488245fb82c027299dd2a2d
gabrielyanarman <armangabrielyan08@gmail.com>
2024-02-04 00:26:00 +0400

    lesson 24 task 3

--- script.js
+++ script.js
@@ -1,80 +1,40 @@
-let table = document.querySelector('table')
-let rows = table.rows
-let td_s = document.querySelectorAll('td')
-let openedTd = false
+let mouse = document.getElementById('mouse')
 
-document.addEventListener('click',function(event) {
-    if(event.target.tagName == 'TEXTAREA') return
-    if(event.target.tagName == 'TABLE') return
-    if(event.target.tagName == 'TBODY') return
-    if(event.target.tagName == 'TR') return
-    if(event.target.tagName == 'BUTTON') return
-    if(event.target.classList.contains('areaTd')) return
+mouse.style.position = 'fixed';
 
-    if(openedTd) {
-       let cancel = document.querySelector('.cancel')
-       cancel.dispatchEvent(new Event('click'))
+mouse.addEventListener('keydown',function(event) {
+    switch(event.key) {
+        case 'ArrowRight' :
+            arrowRight();
+            break;
+        case 'ArrowLeft' :
+            arrowLeft();
+            break;
+        case 'ArrowUp' :
+            arrowUp();
+            break;
+        case 'ArrowDown' :
+            arrowDown();
+            break
     }
-    let target = event.target.closest('TD')
-    target.classList.add('areaTd')
-
-    addArea(target)
-    openedTd = target
 })
 
-function addArea(target) {
-    let textarea = document.createElement('textarea')
-    textarea.value = target.innerHTML
-    let canceledHtml = target.innerHTML
-    textarea.style.border = 0
-    target.innerHTML = '';
-    target.append(textarea)
-
-    addButtons(target)
-
-    let cancel = document.querySelector('.cancel')
-    cancel.addEventListener('click',() => {
-        target.innerHTML = canceledHtml
-        target.classList.remove('areaTd')
-        removeButtons()
-        openedTd = null
-    })
-
-    let edit = document.querySelector('.edit');
-    edit.addEventListener('click',() => {
-        target.innerHTML = textarea.value
-        target.classList.remove('areaTd')
-        removeButtons()
-        openedTd = null
-    })
+function arrowRight() {
+    let coords = mouse.getBoundingClientRect()
+    mouse.style.left = coords.left + mouse.offsetWidth + 'px'
 }
 
-function addButtons(target) {
-    let edit = document.createElement('button')
-    let cancel = document.createElement('button')
-    cancel.className = 'cancel'
-    edit.className = 'edit'
-    let coords = target.getBoundingClientRect()
-
-    edit.textContent = 'OK'
-    cancel.textContent = 'CANCEL'
-
-    edit.style.position = 'absolute'
-	cancel.style.position = 'absolute'
-
-    let left = coords.left 
-    let top = coords.top + target.offsetHeight
-
-    edit.style.left = left + 'px'
-    edit.style.top = top + 'px'
-    cancel.style.left = left + 35 +  'px'
-    cancel.style.top = top + 'px'
-
-    document.body.append(edit)
-    document.body.append(cancel)
+function arrowLeft() {
+    let coords = mouse.getBoundingClientRect()
+	mouse.style.left = coords.left - mouse.offsetWidth + 'px'
 }
 
-function removeButtons() {
-    document.querySelector('.cancel').remove()
-    document.querySelector('.edit').remove()
+function arrowUp() {
+    let coords = mouse.getBoundingClientRect()
+    mouse.style.top = coords.top - mouse.offsetHeight + 'px'
+}
+
+function arrowDown() {
+    let coords = mouse.getBoundingClientRect()
+	mouse.style.top = coords.top + mouse.offsetHeight + 'px'
 }
